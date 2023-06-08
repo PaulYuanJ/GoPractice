082b2c26c29ff9d1a4199eff2a12ae6b33380bed
PaulYuanJ <609417633@qq.com>
2023-06-08 14:51:51 +0800

    init version

---
+++ base/ep-base.go
@@ -0,0 +1,34 @@
+/*
+This package provides golang base practice examples.
+string
+int
+float
+bool
+array
+slice
+map
+struct
+channel
+*/
+package base
+
+import "fmt"
+
+func EP_string() {
+	// define string variables
+
+	// use := to define
+	myString1 := "Hello World!!"
+	fmt.Println(myString1)
+
+	// define the variable type first
+	var myString2 string
+	myString2 = "Hello Golang!"
+	fmt.Println(myString2)
+
+	// use fmt.Sprintf to contact to strings
+	fmt.Println(fmt.Sprintf("%s - %s", myString1, myString2))
+
+	// TODO more examples will come soon...
+
+}
---
+++ base/ep-base_test.go
@@ -0,0 +1,33 @@
+package base
+
+func Example_string() {
+	EP_string()
+}
+
+func Example_int() {
+	EP_string()
+}
+
+func Example_array() {
+	EP_string()
+}
+
+func Example_slice() {
+	EP_string()
+}
+
+func Example_float() {
+	EP_string()
+}
+
+func Example_map() {
+	EP_string()
+}
+
+func Example_struct() {
+	EP_string()
+}
+
+func Example_channel() {
+	EP_string()
+}
